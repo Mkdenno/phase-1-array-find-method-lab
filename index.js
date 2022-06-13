5e3b660145dd56dfe7b56f04ea3096ead2bdce27
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-13 10:14:40 +0300

    commit tree e00ddc79a6c754fcc10e420902d9eda5f2e4a345

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // code your solution here
 
-  superbowlWin=function(record) {
-      for(const item of record){
-          if(item.result==="W"){
-              return item.year;
-          }
-      }
-   }
+//   superbowlWin=function(record) {
+//       for(const item of record){
+//           if(item.result==="W"){
+//               return item.year;
+//           }
+//       }
+//    }
+function superbowlWin(record){
+    function winner(year){
+        return year.result==="W"
+    }
+    const winningYear=record.find(winner);
+        if(winningYear){
+            return winningYear.year;
+        }
+    
+}
   
